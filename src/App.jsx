eafcc81473cee3d5864e5e1a20fd00decfbc6066
Ignė Viktorija <igne.bagdonaite@stud.techin.lt>
2026-03-30 22:42:22 +0300

    added navigation to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { Routes, Route } from "react-router-dom";
 import ComponentsPage from "./pages/ComponentsPage.jsx";
 import UseStatePage from "./pages/UseStatePage.jsx";
 import CrudPage from "./pages/CrudPage.jsx";
+import NavigationComponent from "./components/Header/Navigation/Navigation.jsx";
 import "./App.css";
 
 function App() {
-  <div>
-    <Routes>
-      <Route path="/" element={<ComponentsPage />} />
-      <Route path="/use-state" element={<UseStatePage />} />
-      <Route path="/crud" element={<CrudPage />} />
-    </Routes>
-  </div>;
+  return (
+    <div>
+      <NavigationComponent />
+    
+      <Routes>
+        <Route path="/" element={<ComponentsPage />} />
+        <Route path="/use-state" element={<UseStatePage />} />
+        <Route path="/crud" element={<CrudPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
