bdcebd494c42a3c380b4d649e0c8bb57938d4adc
Ignė Viktorija <igne.bagdonaite@stud.techin.lt>
2026-03-31 14:47:34 +0300

    responsive header and page title

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import ComponentsPage from "./pages/ComponentsPage.jsx";
 import UseStatePage from "./pages/UseStatePage.jsx";
 import CrudPage from "./pages/CrudPage.jsx";
-import NavigationComponent from "./components/Header/Navigation/Navigation.jsx";
 import "./App.css";
 import NotFound from "./pages/Error/ErrorPage.jsx";
+import HeaderComponent from "./components/Header/HeaderComponent.jsx";
 
 function App() {
   return (
     <div>
-      <NavigationComponent />
+
+      <HeaderComponent />
     
       <Routes>
         <Route path="/" element={<ComponentsPage />} />
--- src/components/Header/HeaderComponent.jsx
+++ src/components/Header/HeaderComponent.jsx
@@ -1,9 +1,24 @@
 import NavigationComponent from "./Navigation/Navigation";
 
+let title = "";
+let header = "";
+
 function HeaderComponent() {
+  if (window.location.pathname === "/") {
+    title = "Character Catalog - Components";
+    header = "Character Catalog (Components)";
+  } else if (window.location.pathname === "/use-state") {
+    title = "Character Catalog - useState";
+    header = "Character Catalog (useState)";
+  } else if (window.location.pathname === "/crud") {
+    title = "Character Catalog - CRUD";
+    header = "Character Catalog (CRUD)";
+  }
+  else title = "Error 404 - Not Found";
   return (
     <>
-      <h1>Character Catalog (CRUD)</h1>
+      <title>{title}</title>
+      <h1>{header}</h1>
       <NavigationComponent />
     </>
   );
